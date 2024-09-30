4bee13afedf417af1c3eb62ebc5fcadb2585e430
jeblackburn <jonathan.e.blackburn@gmail.com>
2024-09-29 19:42:05 -0600

    Frontend populating from server.
      - added nginx for reverse proxy
      - uses relative urls for api calls

--- server/src/main/kotlin/com/jeb/kitchensink/KitchenSinkApplication.kt
+++ server/src/main/kotlin/com/jeb/kitchensink/KitchenSinkApplication.kt
@@ -4,8 +4,11 @@ import com.jeb.kitchensink.model.Member
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
 @SpringBootApplication
@@ -17,6 +20,16 @@ class KitchenSinkApplication {
 				Member::class.java
 			)
 		}
+	}
+
+
+}
+
+@Configuration
+class CorsConfig: WebMvcConfigurer {
+	override fun addCorsMappings(registry: CorsRegistry) {
+		println("cors mapping")
+		registry.addMapping("/**")
 	}
 }
 
--- ui/src/components/MembersPage.tsx
+++ ui/src/components/MembersPage.tsx
@@ -1,109 +1,33 @@
-import * as React from 'react';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import Paper from '@mui/material/Paper';
+import { useEffect, useState } from 'react'
+import { DataGrid, GridColDef } from '@mui/x-data-grid'
+import Paper from '@mui/material/Paper'
 import Member from '../members/member.ts'
 
-const members= {
-  "_embedded" : {
-    "members" : [ {
-      "id" : 2,
-      "name" : "John Lennon",
-      "email" : "john@thebeatles.com",
-      "phoneNumber" : "4321432",
-      "_links" : {
-        "self" : {
-          "href" : "http://localhost:8080/api/members/2"
-        },
-        "member" : {
-          "href" : "http://localhost:8080/api/members/2"
-        }
-      }
-    }, {
-      "id" : 3,
-      "name" : "Paul McCartney",
-      "email" : "Paul@thebeatles.com",
-      "phoneNumber" : "4321432",
-      "_links" : {
-        "self" : {
-          "href" : "http://localhost:8080/api/members/3"
-        },
-        "member" : {
-          "href" : "http://localhost:8080/api/members/3"
-        }
-      }
-    }, {
-      "id" : 53,
-      "name" : "Ringo Starr",
-      "email" : "ringo@thebeatles.com",
-      "phoneNumber" : "PA65000",
-      "_links" : {
-        "self" : {
-          "href" : "http://localhost:8080/api/members/53"
-        },
-        "member" : {
-          "href" : "http://localhost:8080/api/members/53"
-        }
-      }
-    }, {
-      "id" : 54,
-      "name" : "Ringo Starr",
-      "email" : "ringo2@thebeatles.com",
-      "phoneNumber" : "PA65000",
-      "_links" : {
-        "self" : {
-          "href" : "http://localhost:8080/api/members/54"
-        },
-        "member" : {
-          "href" : "http://localhost:8080/api/members/54"
-        }
-      }
-    }, {
-      "id" : 103,
-      "name" : "Ringo Starr",
-      "email" : "ringo3@thebeatles.com",
-      "phoneNumber" : "PA65000",
-      "_links" : {
-        "self" : {
-          "href" : "http://localhost:8080/api/members/103"
-        },
-        "member" : {
-          "href" : "http://localhost:8080/api/members/103"
-        }
-      }
-    } ]
-  },
-  "_links" : {
-    "self" : {
-      "href" : "http://localhost:8080/api/members?page=0&size=20"
-    },
-    "profile" : {
-      "href" : "http://localhost:8080/api/profile/members"
-    },
-    "search" : {
-      "href" : "http://localhost:8080/api/members/search"
-    }
-  },
-  "page" : {
-    "size" : 20,
-    "totalElements" : 5,
-    "totalPages" : 1,
-    "number" : 0
-  }
-}
 const columns: GridColDef[] = [
-  { field: 'name', headerName: 'Name', width: 130 },
-  { field: 'email', headerName: "Email Address", width: 130},
-  { field: 'phoneNumber', headerName: "Phone Number", width: 60},
-];
+  { field: 'name', headerName: 'Name', width: 230 },
+  { field: 'email', headerName: 'Email Address', width: 230 },
+  { field: 'phoneNumber', headerName: 'Phone Number', width: 1200 },
+]
 
-const paginationModel = { page: 0, pageSize: 5 };
+const paginationModel = { page: 0, pageSize: 5 }
 
 export default function MembersPage() {
-  const currentMemberList: Member[] = members["_embedded"]["members"];
+  const [currentMembersList, setCurrentMembersList] = useState<Member[]>([])
+
+  useEffect(() => {
+    fetch('/api/members')
+      .then(response => response.json())
+      .then(data => {
+        console.log('got some data', data)
+        const members = data['_embedded']['members'] as Member[]
+        setCurrentMembersList(members)
+      });
+  }, [])
+
   return (
     <Paper sx={{ height: 400, width: '100%' }}>
       <DataGrid
-        rows={currentMemberList}
+        rows={currentMembersList}
         columns={columns}
         initialState={{ pagination: { paginationModel } }}
         pageSizeOptions={[5, 10]}
@@ -111,6 +35,6 @@ export default function MembersPage() {
         sx={{ border: 0 }}
       />
     </Paper>
-  );
+  )
 
 }
